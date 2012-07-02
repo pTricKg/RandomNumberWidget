55225a94f26dfa9a4a05711a2764f1812fd0f1df
pTricKg <ptrickg1976@gmail.com>
2012-07-02 01:42:04 -0600

    initial commit

---
+++ Widget.RandomNumber/src/com/Widget/RandomNumber/WidgetQuestion.java
@@ -0,0 +1,35 @@
+package com.Widget.RandomNumber;
+
+
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+
+public class WidgetQuestion extends AppWidgetProvider {
+
+	private static final String LOG = "com.Widget.RandomNumber";
+
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+
+		Log.w(LOG, "onUpdate method called");
+		// Get all ids
+		ComponentName thisWidget = new ComponentName(context,
+				WidgetQuestion.class);
+		int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+
+		// Build the intent to call the service
+		Intent intent = new Intent(context.getApplicationContext(),
+				WidgetService.class);
+		intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
+
+		// Update the widgets via the service
+		context.startService(intent);
+	}
+}
