a07eb68b696057b1d65f1a64ede74b937416568b
pTricKg <ptrickg1976@gmail.com>
2014-05-27 19:20:45 -0600

    Got rid of hard-coded string

--- Widget.RandomNumber/gen/com/Widget/RandomNumber/R.java
+++ Widget.RandomNumber/gen/com/Widget/RandomNumber/R.java
@@ -27,6 +27,7 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f050001;
         public static final int hello=0x7f050000;
+        public static final int number=0x7f050002;
     }
     public static final class xml {
         public static final int widget_info=0x7f040000;
