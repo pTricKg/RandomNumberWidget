73fa5fd78936ab2836f0c6e7efd7325028151661
pTricKg <ptrickg1976@gmail.com>
2014-06-14 22:48:30 -0600

    Another manifest fix and more

--- Widget.RandomNumber/src/com/Widget/RandomNumber/SetRange.java
+++ Widget.RandomNumber/src/com/Widget/RandomNumber/SetRange.java
@@ -2,8 +2,9 @@ package com.Widget.RandomNumber;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SetRange extends Activity {
 
@@ -11,5 +12,30 @@ public class SetRange extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.range);
+	}
+	
+	public void Range (View v) {
+	// find EditText
+			EditText setRange = (EditText) findViewById(R.id.editText);
+			
+			//Creating string for empty edit text check
+			String checkInput = setRange.getText().toString();
+			try {
+				// checking input
+				if (checkInput.trim().equals("")) {
+					Toast.makeText(getApplicationContext(),
+							"Please enter integer for random number generator", Toast.LENGTH_SHORT).show();
+				}
+				else {
+					Toast.makeText(getApplicationContext(),
+							"Your chose range from 1 to %d", Toast.LENGTH_SHORT).show();
+					// Get range from edit text then make string then parse into long
+					long mRange = Long.parseLong(setRange.getText().toString());
+					
+				}
+			}catch (NumberFormatException e) {
+				Toast.makeText(getApplicationContext(),
+						"Please enter only one integer", Toast.LENGTH_SHORT).show();
+			}
 	}
 }
