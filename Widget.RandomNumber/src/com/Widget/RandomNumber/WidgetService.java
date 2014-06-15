1cc1daecad771d1441b5e8f04b747b601f3e9d9c
pTricKg <ptrickg1976@gmail.com>
2014-06-15 00:08:34 -0600

    Cleaned imports

--- Widget.RandomNumber/src/com/Widget/RandomNumber/WidgetService.java
+++ Widget.RandomNumber/src/com/Widget/RandomNumber/WidgetService.java
@@ -10,7 +10,6 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.RemoteViews;
-import android.widget.Toast;
 
 /**
  * Simple widget gives Random Number
