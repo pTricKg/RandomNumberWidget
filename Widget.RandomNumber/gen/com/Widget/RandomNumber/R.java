67daf2bb1125ecff8c47d0409d30d9291d71d694
pTricKg <ptrickg1976@gmail.com>
2014-06-17 20:33:57 -0600

    Don't even know

--- Widget.RandomNumber/gen/com/Widget/RandomNumber/R.java
+++ Widget.RandomNumber/gen/com/Widget/RandomNumber/R.java
@@ -15,13 +15,12 @@ public final class R {
         public static final int shape=0x7f020001;
     }
     public static final class id {
-        public static final int button1=0x7f060001;
-        public static final int editText=0x7f060002;
-        public static final int editText2=0x7f060005;
-        public static final int layout=0x7f060006;
-        public static final int setR=0x7f060004;
-        public static final int textView1=0x7f060003;
-        public static final int update=0x7f060007;
+        public static final int editText=0x7f060001;
+        public static final int editText2=0x7f060004;
+        public static final int layout=0x7f060005;
+        public static final int setR=0x7f060003;
+        public static final int textView1=0x7f060002;
+        public static final int update=0x7f060006;
         public static final int userinp=0x7f060000;
     }
     public static final class layout {
