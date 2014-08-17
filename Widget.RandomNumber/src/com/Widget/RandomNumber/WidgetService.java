6abc398f1b7cd07e75552ee0a8538d024235645e
pTricKg <ptrickg1976@gmail.com>
2014-08-16 22:58:45 -0600

    Ketchup; working

--- Widget.RandomNumber/src/com/Widget/RandomNumber/WidgetService.java
+++ Widget.RandomNumber/src/com/Widget/RandomNumber/WidgetService.java
@@ -1,35 +1,44 @@
 package com.Widget.RandomNumber;
 
 import java.util.Random;
-
+import android.annotation.SuppressLint;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.RemoteViews;
+import android.widget.TextView;
 
 /*
  * Simple widget gives Random Number
  * 
  * @author Patrick Gorman
  * @version Not Sure
- * Needs work!!
+* Needs work!!
  */
 
+@SuppressLint("NewApi")
 public class WidgetService extends Service {
-
-	private static final String LOG = "com.Widget.RandomNumber";
 	
+	private static final String LOG = "com.Widget.RandomNumber";
+	private static final String TAG = null;
+	private static final int default_key = 100;
+	private static final int RESULT_OK = 1;
+	private static final int GET_TEXT_REQUEST_CODE = 0;
+	TextView mUserTextView;
+
+	// Create some random data
+	//int number = (new Random().nextInt());
 
 	@Override
 	public void onStart(Intent intent, int startId) {
-
+		
 		Log.i(LOG, "Called");
 		
-
 		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this
 				.getApplicationContext());
 
@@ -38,29 +47,33 @@ public class WidgetService extends Service {
 
 		ComponentName thisWidget = new ComponentName(getApplicationContext(),
 				WidgetRandom.class);
-
+		
 		int[] allWidgetIds2 = appWidgetManager.getAppWidgetIds(thisWidget);
-
+		
 		Log.w(LOG, "From Intent" + String.valueOf(allWidgetIds.length));
 		Log.w(LOG, "Direct" + String.valueOf(allWidgetIds2.length));
 
 		for (int widgetId : allWidgetIds) {
-
-			
+						
 			// Create some random data
-			// need to get this from edittext
-			int number = (new Random().nextInt(100));
-
+			Intent i = new Intent();
+			
+			int message = i.getIntExtra("key", 100);
+			System.out.println(message);
+			//int num = Integer.parseInt(message);
+		    //int number = (new Random().nextInt(num));
+			int number = (new Random().nextInt(message));;
+			
 			RemoteViews remoteViews = new RemoteViews(this
 					.getApplicationContext().getPackageName(),
 					R.layout.widget_layout);
-
+			
 			Log.w("WidgetExample", String.valueOf(number));
-
+			
 			// Set the text
 			remoteViews.setTextViewText(R.id.update,
 					"Random # " + String.valueOf(number));
-
+			
 			// Register an onClickListener
 			Intent clickIntent = new Intent(this.getApplicationContext(),
 					WidgetRandom.class);
@@ -74,16 +87,52 @@ public class WidgetService extends Service {
 					PendingIntent.FLAG_UPDATE_CURRENT);
 			remoteViews.setOnClickPendingIntent(R.id.update, pendingIntent);
 			appWidgetManager.updateAppWidget(widgetId, remoteViews);
-
+						
 		}
 		stopSelf();
 
 	}
+
 
 	@Override
 	public IBinder onBind(Intent intent) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
+		Log.i(TAG, "Entered onActivityResult()");
+
+		// TODO - Process the result only if this method received both a
+		// RESULT_OK result code and a recognized request code
+		// If so, update the Textview showing the user-entered text.
+		if (requestCode == GET_TEXT_REQUEST_CODE) {
+			if (resultCode == RESULT_OK) { // So this was checking requestCode
+											// before
+											// obviously, that didnt run code
+											// inside
+				// this code check for data received from other activities
+				// intent
+				// if (data == null) {
+				// return;
+				// }
+				// Instead of creating new intent, I used Intent data and it now works.
+				// Getting data from edittext input from other activity
+				String message = data.getStringExtra("key");
+				mUserTextView.setText(message);
+
+			}
+
+		}
+	}
+
+
+	public static int getDefaultKey() {
+		return default_key;
+	}
+
+	
 }
+
+
